ac0fd4e0dcf0ae6f06a9b2506417f1434eae3eca
joshuayoes <37849890+joshuayoes@users.noreply.github.com>
2018-08-19 22:38:07 -0700

    Recovery for final version
    recovered first two paragraphs from finished version
    
    tidyed up names of files and variables. made sure all of the files point to each other

--- base10_base2/base10_base2.js
+++ base10_base2/base10_base2.js
@@ -1,17 +1,5 @@
-
-/*
-console.log(keyboard);
-keyboard.forEach(function(key){
-  keys.insertAdjacentHTML('beforeend', `${key.ascii} `);
-});
-*/
-/*
-keyboard.forEach(function(key){
-  keys.insertAdjacentHTML('beforeend', `${key.ascii.toString(2)} = ${key.ascii} = ${key.letter} <br>`);
-});
-*/
 //create HTML elements
-const keys = document.getElementsByClassName('keys')[0];
+const base10Base2 = document.getElementsByClassName('base10_base2')[0];
 const form = document.createElement('form');
 const numField = document.createElement('input');
 const binaryField = document.createElement('input');
@@ -21,15 +9,12 @@ const binaryHeading = document.createElement('h2');
 const binaryHeadingText = document.createTextNode('Base 2');
 const diagram = document.createElement('img');
 const baseCounting = document.createElement('p');
-//const baseCountingText = document.createTextNode()
 
 //set input attributes
 diagram.setAttribute('src', 'img/number_diagram.svg')
 diagram.setAttribute('alt', 'Diagram visualizing difference between base 2 and base 10 counting')
-numField.setAttribute('type', 'text');
-binaryField.setAttribute('type', 'text');
-numField.setAttribute('pattern', '[0-9]/g');
-binaryField.setAttribute('pattern', '[0-9]/g');
+numField.setAttribute('type', 'number');
+binaryField.setAttribute('type', 'number');
 numField.setAttribute('value', '4');
 binaryField.setAttribute('value', '100');
 binaryField.setAttribute('readonly','readonly');
@@ -39,8 +24,8 @@ binaryHeading.classList.add('base2__h2')
 binaryField.classList.add('base2');
 
 //setting HTML element structure
-keys.appendChild(diagram);
-keys.appendChild(form);
+base10Base2.appendChild(diagram);
+base10Base2.appendChild(form);
 
 numHeading.appendChild(numHeadingText);
 binaryHeading.appendChild(binaryHeadingText);
@@ -50,27 +35,8 @@ form.appendChild(numField);
 form.appendChild(binaryHeading);
 form.appendChild(binaryField);
 
-//updates base 2 form based on base 10 change
 numField.addEventListener('input', function(event){
-  let num = this.value;
+  let num = numField.value;
   //converts base 10 number into binary
   binaryField.value = parseInt(num).toString(2);
-});
-
-const base10value = document.getElementsByClassName('base10value')[0];
-const base2value = document.getElementsByClassName('base2num')[0];
-const buttonUp = document.getElementsByClassName('buttonup')[0];
-const buttonDown = document.getElementsByClassName('buttondown')[0];
-const
-
-base10value.innerText = '10'
-
-buttonUp.addEventListener('click', () => {
-  base10value.innerText++;
-  base2value.innerText = parseInt(base10value.innerText).toString(2);
-});
-
-buttonDown.addEventListener('click', () => {
-  base10value.innerText--;
-  base2value.innerText = parseInt(base10value.innerText).toString(2);
 });
